ea483f2dd7f0b5c139005164ff4339f58177c903
Euclidean13 <aacristellys@icloud.com>
2020-05-24 22:15:46 +0200

    master: Integration test for posting and updating a new Library event

--- library-events-consumer/src/main/java/com/learnkafka/entity/Book.java
+++ library-events-consumer/src/main/java/com/learnkafka/entity/Book.java
@@ -9,8 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
