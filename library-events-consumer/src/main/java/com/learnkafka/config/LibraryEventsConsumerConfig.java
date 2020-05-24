5db7d8f90aeabd88cc5052b6a74156fbbaa766af
Euclidean13 <aacristellys@icloud.com>
2020-05-24 16:55:49 +0200

    master: Adds H2 DB for persisting library events

--- library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -20,7 +20,7 @@ public class LibraryEventsConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory);
         // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-
+        // factory.setConcurrency(3);
         return factory;
     }
 }
